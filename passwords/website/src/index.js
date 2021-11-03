8c70796a17bd417210a06bfee594a5e80c5fca49
fleischgewehr <thesickboii@gmail.com>
2021-11-04 01:02:47 +0200

    set up front end routing

--- passwords/website/src/index.js
+++ passwords/website/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from './App'
+import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
+import Login from "./components/Login/Login";
+
+const rootElement = document.getElementById("root");
+render(
+    <BrowserRouter>
+        <Link to="/">Homepage</Link>{" "}
+        <Link to="/login">Login</Link>{" "}
+        <Link to="/register">Register</Link>
+        <Routes>
+            <Route path="/" element={<App />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<RegistrationForm />}></Route>
+        </Routes>
+    </BrowserRouter>,
+    rootElement,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
