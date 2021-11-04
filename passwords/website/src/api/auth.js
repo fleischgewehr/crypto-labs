b272295b1fcde12b4d10969be67a28057b4f8f06
fleischgewehr <thesickboii@gmail.com>
2021-11-04 02:08:35 +0200

    fix api urls

--- passwords/website/src/api/auth.js
+++ passwords/website/src/api/auth.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const login = async (username, password) => {
     try {
-        return await axios.post('localhost:8080/users/login', { username, password });
+        return await axios.post('http://localhost:8080/users/login', { username, password });
     } catch (e) {}
 }
 
 const register = async (username, password) => {
     try {
-        return await axios.post('localhost:8080/users', { username, password });
+        return await axios.post('http://localhost:8080/users', { username, password });
     } catch (e) {}
 }
 
